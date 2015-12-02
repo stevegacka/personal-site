42bb484695cb6494a6ba340d95961876342a8b16
Steve Gacka <steve.gacka@startups.co>
2015-12-01 21:26:32 -0500

    recursive copy

--- deploy/production.sh
+++ deploy/production.sh
@@ -5,6 +5,6 @@ SITE=personal-site
 cp /var/projects/$SITE/server/* /etc/apache2/sites-enabled/
 service apache2 restart
 rm -rf /var/www/$SITE/*
-cp /var/projects/$SITE/* /var/www/$SITE/
+cp /var/projects/$SITE/* /var/www/$SITE/ -R
 
 echo "Done!"
